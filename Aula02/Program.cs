6115454143524faac249f3940b6084408e69902e
José Amando Marques <joseamando5974@gmail.com>
2025-08-07 22:07:53 -0300

    commit aula 02

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -9,7 +9,7 @@ namespace MyApp
             Console.WriteLine("Zekinha");
             if(args.GetLength(0)>0)
             {
-                Console.Write(args.GetValue(0));
+                Console.WriteLine(args.GetValue(0));
             }
         }
     }
