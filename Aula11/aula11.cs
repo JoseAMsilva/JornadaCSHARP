88fd617ece351f91b6ba7522e473add73997ad8c
José Amando Marques <joseamando5974@gmail.com>
2025-08-07 22:04:30 -0300

    Commit aula 11

---
+++ Aula11/aula11.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Principal
+{
+    static void Main()
+    {
+        int Vint = 10;
+        short Vshort = (short)Vint;//Type Cast
+
+        Console.WriteLine(Vshort);
+    }
+}
