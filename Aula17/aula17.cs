8ffe95ebaf1d195e8ef7abef4301360b49996595
José Amando Marques <joseamando5974@gmail.com>
2025-08-08 17:54:28 -0300

    commit aula 17

---
+++ Aula17/aula17.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Principal {
+    static void Main()
+    {
+        int[] vetor = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        string[] veiculos = { "Carro", "Avião", "Ônibus" };
+
+        Console.WriteLine(veiculos[0]);
+        Console.WriteLine(veiculos[1]);
+        Console.WriteLine(veiculos[2]);
+    }    
+}
