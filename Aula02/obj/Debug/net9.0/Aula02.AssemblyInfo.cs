2298b146bcd3a8c780a7445030df0c1c00ae0e5e
José Amando Marques <joseamando5974@gmail.com>
2025-08-16 19:28:44 -0300

    commit C#

--- Aula02/obj/Debug/net9.0/Aula02.AssemblyInfo.cs
+++ Aula02/obj/Debug/net9.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38f869027b6c486f632a72e4f0d2d12392419e3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4090554675321b64f37db064b633b03079ffc72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Aula47/aula47.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class Calcular
+{
+    public int Somar(params int[] num)
+    {
+        int soma = 0;
+        foreach (int n in num)
+        {
+            soma += n;
+        }
+        return soma;
+    }
+    public double Somar(params double[] num)
+    {
+        double soma = 0;
+        foreach (double n in num)
+        {
+            soma += n;
+        }
+        return soma;
+    }
+}
+public class Aula27
+{
+    static void Main()
+    {
+        double soma;
+        Calcular calculo = new Calcular();
+        soma = calculo.Somar(10, 20.546, 33.53, 47);
+        Console.WriteLine(soma);
+    }
+}
