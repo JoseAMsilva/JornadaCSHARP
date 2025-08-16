740d326afa4711980e4316accde57cdfebbab144
José Amando Marques <joseamando5974@gmail.com>
2025-08-15 22:27:59 -0300

    commit C#

---
+++ Aula46/aula46.cs
@@ -0,0 +1,55 @@
+using System;
+
+public class Galinha
+{
+    private string Nome;
+    private int TotalOvos;
+    public Galinha(string Nome)
+    {
+        this.Nome = Nome;
+        this.TotalOvos = 0;
+    }
+    public Ovo botar()
+    {
+        this.TotalOvos++;
+        return new Ovo(this.TotalOvos, this.Nome);
+    }
+    public void Info()
+    {
+        Console.WriteLine("Nome da Galinha: {0}", this.Nome);
+        Console.WriteLine("Total de Ovos: {0}", this.TotalOvos);
+        Console.WriteLine("-------------------------");
+    }
+}
+public class Ovo
+{
+    public int QntdeOvos;
+    public string MinhaGalinha;
+    public Ovo(int QntdeOvos, string MinhaGalinha)
+    {
+        this.QntdeOvos = QntdeOvos;
+        this.MinhaGalinha = MinhaGalinha;
+        Console.WriteLine("Ovo {0} da Galinha {1}", this.QntdeOvos, this.MinhaGalinha);
+        Console.WriteLine("-------------------------");
+    }
+}
+public class Aula46
+{
+    static void Main()
+    {
+        Galinha G1 = new Galinha("Clotilda");
+        Galinha G2 = new Galinha("Margarida");
+        Galinha G3 = new Galinha("Pintadinha");
+
+        G1.botar();
+        G1.botar();
+        G2.botar();
+        G3.botar();
+        G3.botar();
+        G3.botar();
+        G1.Info();
+        G2.Info();
+        G3.Info();
+    }
+}
+    
